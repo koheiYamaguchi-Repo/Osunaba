947f4c831cb06fc724bc658163af5d6a689d9348
KouheiYamaguchi <messala2319@gmail.com>
2021-02-14 13:08:40 +0900

    added:pageView追加 added:データ表示用のクラスを追加(コメントアウト)

--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
 // import 'package:flappy_search_bar/flappy_search_bar.dart';
 
 class Home extends StatelessWidget {
+  final controller = PageController(
+    initialPage: 1,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              // child: SearchBar(),
-            ),
+      body: SafeArea(
+          //検索ボックスの配置
+          //フィルター、ソートボタンの配置
+        child: PageView(
+          controller: controller,
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            //カードの金型
+              //
+          ],
         ),
+      ),
     );
   }
 }
+
+//取得、表示用のクラスを呼び出し
+//データの数だけ生成
+// class Contents extends StatefulWidgetBuilder{
+//   @override
+//   //上下にスワイプされた時の動作を検知して次のデータをロード
+// }
